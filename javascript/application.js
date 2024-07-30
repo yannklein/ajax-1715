6836303ad71f6a240915caa057c59515c7058c32
Yann Klein <yann.klein@me.com>
2024-07-30 10:30:16 +0900

    lecture done

--- javascript/application.js
+++ javascript/application.js
@@ -1,76 +1,88 @@
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 console.log("Hello from JavaScript!");
 
-const url = "https://api.github.com/users/littlebobert";
-
-
 // FETCH GET REQUEST example
-// fetch(url).then((response) => {
-//   console.log(response);
-//   console.log(response.status);
-//   // response.json() converts raw JSON file into JS code (array/object)
-//   // response.json() returns a Promise
-//   return response.json()
-// }).then((userInfo) => {
-//   console.log(userInfo.name);
-// });
-// console.log("after the fetch");
+const url = "https://api.github.com/users/jpblicer";
+
+// console.log(fetch(url));
+const result = fetch(url); // DOESN'T GIVE US  THE request result
+
+const doSomething = (data) => {
+  // ...
+}
+
+fetch(url)
+  .then((response) => {
+    // console.log("response:", response);
+    // console.log(response.json());
+    return response.json();
+  })
+  .then((data) => {
+    // console.log(data);
+    // console.log("the data arrives!", data);
+    doSomething(data)
+  });
+
+// console.log("after the fetch"); // the fetched data does NOT exist yet!
 
 fetch(url)
   .then(response => response.json())
-  .then((userInfo) => {
-    console.log(userInfo.name);
-});
+  .then((data) => {
+    // console.log(data);
+  })
 
-// {
-//   "email": "eve.holt@reqres.in",
-//   "password": "pistol"
-// }
+
+
 
 // FETCH POST REQUEST example
-const postUrl = "https://reqres.in/api/register"
 
-// 1. Select elements (2 inputs, form)
-const email = document.querySelector("#email");
-const password =  document.querySelector("#password");
-const form = document.querySelector("#form");
+// 1. Select elements (inputs, login)
+const emailEl = document.querySelector("#email");
+const passwordEl = document.querySelector("#password");
+const buttonEl = document.querySelector("#button");
 
-// 2. Listen to a submit on the form
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+// 2. Listen to a click on login
+buttonEl.addEventListener("click", (event) => {
   console.log(event);
-  // 2.5 Fetch the reqres API (POST)
+  event.preventDefault();
+  // 2.5 Fetch the dummy API
+  const url = "https://reqres.in/api/register";
+
   const data = {
-    email: email.value,
-    password: password.value
+    email: emailEl.value,
+    password: passwordEl.value
   }
-  fetch(postUrl, {
+
+  const options = {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data) // converts JS data into JSON file
-  }).then((response) => {
-    if (response.status === 200) {
-      // show a success alert
-      Swal.fire({
-        position: "top-end",
-        icon: "success",
-        title: "Your work has been saved",
-        showConfirmButton: false,
-        timer: 1500
-      });
-    } else {
-      // show a error alert
-      Swal.fire({
-        title: 'Error!',
-        text: 'Do you want to continue',
-        icon: 'error',
-        confirmButtonText: 'Cool'
-      })
-    }
-  })
-})
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify(data)
+  }
+
+  fetch(url, options)
+    .then((response) => {
+      if (response.status === 200) {
+        Swal.fire({title: 'Success', text: 'You are connected', icon: 'success'})
+      } else {
+        Swal.fire({title: 'Error!', text: 'Oops! Something went wrong', icon: 'error'})
+      }
+      return response.json()
+    })
+    .then((data) => {
+      // 3. Console.log the data
+      console.log(data);
+    })
+  
+});
 
 
+// SWEETALERT example
 
-
+// Swal.fire({
+//   // position: "bottom-start",
+//   icon: "success",
+//   title: "Your work has been saved",
+//   showConfirmButton: false,
+//   timer: 1500
+// });
